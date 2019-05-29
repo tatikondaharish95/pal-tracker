a49995c3424e7c869cd79d4ff9813678ef5a697e
kshitij-chhatwani <kc925@scarletmail.rutgers.edu>
2019-05-29 11:27:34 -0400

    Updated and added Manifest.yml, Controllers and test files

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,12 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-    @GetMapping("/")
-    public String sayHello(){
-        return "hello";
+
+    private String welcome_message = "hello default";
+
+    public WelcomeController(@Value("${welcome.message}") String welcome_message) {
+        this.welcome_message = welcome_message;
+    }
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return welcome_message;
     }
 }
